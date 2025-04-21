a483a2a4858daa9a6a3bd31acd185df62bc64295
Mario Miličević <mmilic05@fesb.hr>
2025-04-21 20:12:41 +0200

    cleanup

--- src/main/java/com/ht/interview/service/TechnicianServiceImpl.java
+++ src/main/java/com/ht/interview/service/TechnicianServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.data.domain.Pageable;
 //import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+//import java.util.List;
 
 @Service
 public class TechnicianServiceImpl implements TechnicianService {
