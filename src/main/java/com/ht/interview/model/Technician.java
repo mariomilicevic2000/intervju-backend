1131e1f1eab8f48cbac73cbca036af21038e09f4
Mario Miličević <mmilic05@fesb.hr>
2025-04-21 20:13:10 +0200

    added data validation

--- src/main/java/com/ht/interview/model/Technician.java
+++ src/main/java/com/ht/interview/model/Technician.java
@@ -12,13 +12,13 @@ public class Technician {
     private UUID id;
 
     @NotNull(message = "KP broj je obavezan")
-    @Pattern(regexp = "\\d{3,10}")
+    @Pattern(regexp = "\\d{3,10}", message = "KP broj mora biti izmedu 3 i 10 znamenaka")
     private String kpNumber;
     @NotNull(message = "Ime ne smije biti prazno")
-    @Size(min = 2, max = 50)
+    @Size(min = 2, max = 50, message = "Ime mora imati barem 2 slova")
     private String firstName;
     @NotNull(message = "Prezime ne smije biti prazno")
-    @Size(min = 2, max = 50)
+    @Size(min = 2, max = 50, message = "Prezime mora imati barem 2 slova")
     private String lastName;
 
     @ManyToOne
@@ -27,19 +27,26 @@ public class Technician {
     private GroupManager groupManager;
 
     @NotNull(message = "Kontakt telefon mora biti definiran")
+    @Pattern( regexp = "^(\\+385|0)(91|92|95|97|98|99)[\\s-]?\\d{3}[\\s-]?\\d{3,4}$", message = "Unesite ispravan hrvatski mobilni broj"
+    )
     private String contactMobile;
     @NotNull(message = "Email mora biti definiran")
-    @Email
+    @Email(message = "Unesite ispravnu email adresu")
     private String contactEmail;
 
     @NotNull(message = "Adresa mora biti definirana")
+    @Pattern(regexp = "^[A-Za-zČčĆćŽžŠšĐđ]{2,}(\\s[A-Za-zČčĆćŽžŠšĐđ]{1,})*$", message = "Unesite ispravnu hrvatsku adresu")
     private String workStreetName;
 
     @NotNull(message = "Kucni broj mora biti definiran")
+    @Size(min = 1, max = 5, message = "Kucni broj mora imati izmedu 1 i 5 znakova")
+    @Pattern(regexp = "^\\\\d{1,5}[A-Za-z]?$|^bb$", message = "Unesite ispravan kucni broj")
     private String workStreetNumber;
     @NotNull(message = "Postanski broj mora biti definiran")
+    @Pattern(regexp = "^[1-5]\\d{4}$", message = "Unesite ispravan hrvatski postanski broj")
     private String workPostcode;
     @NotNull(message = "Mjesto mora biti definirano")
+    @Pattern(regexp = "^[A-Za-zČĆŽŠĐčćžšđ\\s\\-']+$", message = "Unesite ispravno ime naselja")
     private String workCity;
 
     public UUID getId() {
