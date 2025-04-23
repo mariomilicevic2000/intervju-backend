3d9692a32aab0bc7d7256d23dfee23c5f2b685c4
Mario Miličević <mmilic05@fesb.hr>
2025-04-23 15:10:16 +0200

    lifted CORS config to separate class

--- src/main/java/com/ht/interview/controller/GroupManagerController.java
+++ src/main/java/com/ht/interview/controller/GroupManagerController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 public class GroupManagerController {
     private final GroupManagerService groupManagerService;
 
--- src/main/java/com/ht/interview/controller/TechnicianController.java
+++ src/main/java/com/ht/interview/controller/TechnicianController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 public class TechnicianController {
 
     private final TechnicianService technicianService;
